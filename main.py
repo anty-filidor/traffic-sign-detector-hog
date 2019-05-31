b72ec12218d19d456b49bba650a4abb0b5c05705
michal <michal.czuba.1995@gmail.com>
2019-05-31 19:22:12 +0200

    Added MAP calculations (alpha)

--- main.py
+++ main.py
@@ -9,80 +9,20 @@ import pickle
 import pandas as pd
 
 
-def test_on_images():
+def detect_on_images(save_images=True, with_bboxes=True):
+    """
+    This method performs tests HOG+SVM detector on the images in given folder it also prepares data
+     to calculate mean average precision
+    :param save_images: if true method saves images with marked rois
+    :param with_bboxes: if true method add to path_to_output_images_dir.pkl ground truth rois of procesed image
+    (of course a file with gt rois is obligatory )
+    """
     print(colored("Starting processing images\n", 'red'))
 
     # create sliding window object
     sliding_window_parameters = {
-        'x_win_len': 60,
-        'y_win_len': 60,
-        'x_increment': 40,
-        'y_increment': 40,
-        'svc_path': 'trained_models/SVC_2019527.pkl',
-        'scaler_path': 'trained_models/scaler_2019527.pkl',
-        'downscale_for_pyramid': 1.3
-    }
-    sw = SlidingWindow(sliding_window_parameters, binary_detection=True)
-    print(colored("Created SlidingWindow object\n", 'red'))
-
-    # create directories to images
-    path_to_test_images_dir = './dataset/test_images/'
-    path_to_output_images_dir = path_to_test_images_dir + 'output/'
-    path_to_test_images_dir = '/Users/michal/Tensorflow/datasets/GTSDB/'
-    test_image_paths = glob.glob(path_to_test_images_dir + '*.ppm')
-
-    # if there is no folder "output" create it
-    if not os.path.exists(path_to_output_images_dir):
-        subprocess.run(['mkdir', path_to_output_images_dir])
-        print(colored(("Created directory: {}\n".format(path_to_output_images_dir)), 'red'))
-
-    # cerate list for output logs to further MAP (or IoU) calculation
-    detections_output_result = []
-
-    # process all images in folder
-    print(colored("Processing:\n", 'red'))
-    for image_path in tqdm(test_image_paths[:20]):
-
-        # name = image_path.split('/')[3]
-        name = image_path.split('/')[6]
-        image = cv2.imread(image_path)
-
-        sw.update_frame(image)
-        image, pred_bboxes, pred_classes, confidences = sw.perform()
-
-        cv2.imwrite(path_to_output_images_dir + name, image)
-
-        logs = {
-            'file': name,  # filename
-            'file_path': path_to_test_images_dir,  # path to file
-
-            'gt_classes': [0],  # ground truth classes labels list
-            'gt_bboxes': [0],  # list of ground truth bounding boxes
-
-            'pred_classes': pred_classes,  # predicted classes labels list
-            'pred_bboxes': pred_bboxes,  # list of predicted bounding boxes (bb are tuples)
-            'confidences': confidences,  # list of prediction confidences
-        }
-        detections_output_result.append(logs)
-
-    # Pickle the 'detections_output_result' list using the highest protocol available.
-    with open(path_to_output_images_dir + 'detections_output_result.pkl', 'wb') as f:
-        pickle.dump(detections_output_result, f, pickle.HIGHEST_PROTOCOL)
-        f.close()
-
-    with open(path_to_output_images_dir + 'sliding_windows_params.txt', 'w') as f:
-        f.write(str(sliding_window_parameters))
-        f.write(str(sw._object_detector._hog_parameters))
-        f.close()
-
-
-def prepare_measurable_data(save_images=True):
-    print(colored("Starting processing images\n", 'red'))
-
-    # create sliding window object
-    sliding_window_parameters = {
-        'x_win_len': 60,
-        'y_win_len': 60,
+        'x_win_len': 50,
+        'y_win_len': 50,
         'x_increment': 40,
         'y_increment': 40,
         'svc_path': 'trained_models/SVC_2019527.pkl',
@@ -99,12 +39,12 @@ def prepare_measurable_data(save_images=True):
     test_image_paths = glob.glob(path_to_test_images_dir + '*.ppm')
 
     # read csv with ground true bounding boxes
-    path_to_gt_txt = '/Users/michal/Tensorflow/datasets/GTSDB/gt.txt'
-    gt = pd.read_csv(path_to_gt_txt, delimiter=';', header=None,
-                     names=['file', 'x1', 'y1', 'x2', 'y2', "class_id"])
+    if with_bboxes:
+        path_to_gt_txt = '/Users/michal/Tensorflow/datasets/GTSDB/gt.txt'
+        gt = pd.read_csv(path_to_gt_txt, delimiter=';', header=None, names=['file', 'x1', 'y1', 'x2', 'y2', "class_id"])
 
-    # if binary classification uncomment line below
-    gt['class_id'] = 1
+        # if binary classification uncomment line below
+        gt['class_id'] = 1
 
     # if there is no folder "output" create it
     if not os.path.exists(path_to_output_images_dir):
@@ -116,15 +56,20 @@ def prepare_measurable_data(save_images=True):
 
     # process all images in folder
     print(colored("Processing:\n", 'red'))
-    for image_path in tqdm((test_image_paths[:25])):
+    for image_path in tqdm((test_image_paths[:200])):
 
+        # name = image_path.split('/')[3]
         name = image_path.split('/')[6]
         image = cv2.imread(image_path)
 
         # search for selected image's rows in the csv and extract rois and id of classes
-        bb = (gt.loc[gt['file'] == name])
-        classes = bb['class_id'].values.tolist()
-        gt_bboxes = [tuple(l) for l in bb[['x1', 'y1', 'x2', 'y2']].values]
+        if with_bboxes:
+            bb = (gt.loc[gt['file'] == name])
+            classes = bb['class_id'].values.tolist()
+            gt_bboxes = [tuple(l) for l in bb[['x1', 'y1', 'x2', 'y2']].values]
+        else:
+            classes = [0]
+            gt_bboxes = [0]
 
         # perform detection
         sw.update_frame(image)
@@ -158,5 +103,4 @@ def prepare_measurable_data(save_images=True):
         f.close()
 
 
-# test_on_images()
-prepare_measurable_data(save_images=True)
+detect_on_images(save_images=True, with_bboxes=True)
